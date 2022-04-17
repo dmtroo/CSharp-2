34961abf2f4afad0a91e1d10b201040ce62d3252
Dmytro Parnak <dmparnak@gmail.com>
2022-04-17 11:28:39 +0300

    Return calender to previous saved date after wrong birthdate message

--- Lab02/ViewModels/PersonViewModel.cs
+++ Lab02/ViewModels/PersonViewModel.cs
@@ -119,17 +119,26 @@ namespace ProgrammingInCSharp.Lab02.ViewModels
 
         private async void Proceed()
         {
+            DateTime? oldBirthdate = DateTime.Today;
+            if (_person != null && _person.Birthdate < DateTime.Today) 
+                oldBirthdate = _person.Birthdate;
+
             _person = new Person(Name, Surname, Email, Birthdate);
 
             switch (_person.Age)
             {
                 case < 0:
-                    MessageBox.Show($"Wrong birthdate!\n -- You haven't been born yet --");
+                    MessageBox.Show($"Wrong birthdate!\n -- You haven't been born yet --\n(age must be higher than 0)");
+                    ReturnDate(oldBirthdate);
                     return;
                 case > 135:
-                    MessageBox.Show($"Wrong birthdate!\n -- You are probably dead -- \n must be less than 135");
+                    MessageBox.Show($"Wrong birthdate!\n -- You are probably dead -- \n(age must be less than 135)");
+                    ReturnDate(oldBirthdate);
                     return;
             }
+
+            if (_person.IsBirthday)
+                MessageBox.Show($"Happy birthday!!!");
 
             IsEnabled = false;
             await Task.Run(() => Calculate());
@@ -149,9 +158,12 @@ namespace ProgrammingInCSharp.Lab02.ViewModels
             OnPropertyChanged(nameof(Surname));
             OnPropertyChanged(nameof(Email));
 
-            if (_person.IsBirthday)
-                MessageBox.Show($"Happy birthday!!!");
+        }
 
+        private void ReturnDate( DateTime? oldBirthdate)
+        {
+            Birthdate = oldBirthdate;
+            OnPropertyChanged(nameof(Birthdate));
         }
 
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
